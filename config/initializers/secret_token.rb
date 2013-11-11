b191b5401a0048480d41f9060e5652fc98a6b6f9
Ryan Page <rpage326@gmail.com>
2013-11-11 14:46:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharitableApp::Application.config.secret_key_base = '916695c907e28995ac90e3580ceeffa8340deeba63cfba9053121338d17eca8f90bd9b106bbce457182017c2f7a13ff44a801435c05619e4273d5750111cd97a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CharitableApp::Application.config.session_store :cookie_store, key: '_charitable_app_session'
