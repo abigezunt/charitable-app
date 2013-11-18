c90036d1a6facf5134d04f475878c067db499b01
Abby Howell <abby.g.howell@gmail.com>
2013-11-18 18:16:45 -0500

    added JasmineRails to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CharitableApp::Application.routes.draw do
  
 root 'locations#index'
+
+mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
