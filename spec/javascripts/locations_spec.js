c45657e8615613f4d829de02d5860e2d5b6af95b
Abby Howell <abby.g.howell@gmail.com>
2013-11-18 18:31:20 -0500

    jasmine tests not working yet

--- spec/javascripts/locations_spec.js
+++ spec/javascripts/locations_spec.js
@@ -1,8 +1,8 @@
 // include spec/javascripts/helpers/spec_helper.js and app/assets/javascripts/foo.js
 //= require helpers/spec_helper
 //= require locations
-describe('Foo', function() {
-  it("does something", function() {
-    expect(1 + 1).toBe(2);
+describe('Map', function() {
+  it("recognizes what you clicked", function() {
+    expect($('#clicked-state').effect).toBe('highlight', {color: '#C7F464'}, 2000);
   });
 });
